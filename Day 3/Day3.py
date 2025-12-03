d1501dc25033b421f471b1ffa905eee8d5f8ad9e
Clarisa Castillo <clarisa@epic.com>
2025-12-03 15:04:03 -0600

    Updating comments and small refinements to my solution

--- Day 3/Day3.py
+++ Day 3/Day3.py
@@ -25,30 +25,30 @@ def sum_joltages(data: str, j_len: int) -> int:
 
     # Generator to yield joltages from each pack in the input data
     def joltages():
-        for line in data.splitlines():
-            yield find_joltage(line, j_len)
+        for pack in data.splitlines():
+            yield find_joltage(pack, j_len)
 
     return sum(joltages()) # sum all joltages generated from the input data
 
 # Find the largest joltage by selecting j_len batteries from the given pack line
-def find_joltage(line: str, j_len: int) -> int:
+def find_joltage(pack: str, j_len: int) -> int:
     joltage = [0 for _ in range(j_len)] # initialize list to hold selected batteries as strings (for easier concatenation later)
     loc = 0 # initialize starting location for search
-    L = len(line) # length of the battery pack line
+    L = len(pack) # length of the battery pack line
     for i in range(j_len):
         # for each battery left to select, find the largest battery in the remaining range of the line.
         # This range shrinks as we select more batteries, since we need to leave enough batteries at the end to fill the remaining slots.
-        # The next battery is chosen from the range (loc + 1) to (L + 1 - j_len + i) of the pack, where loc is the position of the last found battery.
-        joltage[i], loc = find_largest_in_range(line, loc, L + 1 - j_len + i)
+        # The next battery is chosen from the range (loc + 1) to (L + 1 - j_len + i) of the pack, where loc is the position of the last found battery, and i is the index of the current battery being selected.
+        joltage[i], loc = find_largest_in_range(pack, loc, L + 1 - j_len + i)
 
     return int("".join(joltage)) # concatenate selected batteries and convert to int
 
-# For each given search range, find the largest battery and return it along with its position for the next search
-def find_largest_in_range(line: str, start: int, end: int) -> int:
+# For each given search range, find the largest battery and return it along with its position which will be the starting location in the next search
+def find_largest_in_range(pack: str, start: int, end: int) -> int:
     largest = "0" # initialize largest found battery
     delta = 0 # this will track the offset from start to the position of the found largest battery to be used in the next search
     # search for the largest battery in the specified range of the pack line
-    for idx, x in enumerate(line[start:end]):
+    for idx, x in enumerate(pack[start:end]):
         if int(x) > int(largest):
             largest = x # update largest found battery
             delta = idx # delta from start to the position of the found largest battery
